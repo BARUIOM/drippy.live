f753422f100af022281ff9a74351ce763aa9b3a9
VitorMac10 <vitormac3@hotmail.com>
2019-12-31 21:57:23 -0300

    New authentication/registration method

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,6 +15,10 @@
                         <label>Password</label>
                         <md-input type="password" v-model="password" required />
                     </md-field>
+                    <md-field v-if="newUser">
+                        <label>Username</label>
+                        <md-input type="text" v-model="username" required />
+                    </md-field>
                     <md-checkbox v-model="newUser">I don't have an account</md-checkbox>
                 </md-card-content>
 
@@ -39,57 +43,44 @@ export default {
     data: () => ({
         email: '',
         password: '',
+        username: '',
         newUser: false,
         message: '',
         showSnackbar: false,
     }),
     methods: {
         async doLogin() {
+            this.showSnackbar = false;
             this.$refs.submit.$el.setAttribute('disabled', 'true');
-            if (this.newUser) {
-                const response = await this.axios({
-                    url: '/register',
-                    baseURL: this.$root.api_url,
-                    method: 'POST',
-                    data: { email: this.email, password: this.password }
-                });
+            try {
+                if (this.newUser) {
+                    await this.axios({
+                        url: '/register',
+                        baseURL: this.$root.api_url,
+                        method: 'POST',
+                        data: { email: this.email, password: this.password, username: this.username }
+                    });
 
-                if (response.data['error'] && response.data['error']['message'] == "EMAIL_EXISTS") {
-                    this.message = 'E-Mail already registered!';
-                } else if (response.data['kind'] && response.data['kind'] == 'identitytoolkit#GetOobConfirmationCodeResponse') {
-                    this.message = 'User successfully registered, now please verify your e-mail!';
                     this.newUser = false;
-                }
-                this.showSnackbar = true;
-            } else {
-                try {
+                    this.message = 'User successfully registered, now please verify your e-mail!';
+                    this.showSnackbar = true;
+                } else {
                     const response = await this.axios({
                         url: '/login',
                         baseURL: this.$root.api_url,
                         method: 'POST',
                         data: { email: this.email, password: this.password }
                     });
-
-                    if (response.data['error']) {
-                        if (response.data['error']['message'] == "INVALID_PASSWORD") {
-                            this.message = 'Invalid password provided!';
-                        } else if (response.data['error']['message'] == "EMAIL_NOT_FOUND") {
-                            this.message = "This account wasn't registered yet!";
-                        }
-
-                        this.showSnackbar = true;
-                    } else if (response.data['idToken']) {
-                        this.$emit('set-user', response.data);
-                    }
-                } catch (error) {
-                    if (error.response && error.response.status == 403) {
-                        this.message = "Please verify your e-mail inbox!";
-                        this.showSnackbar = true;
-                    }
+                    this.$emit('set-user', response.data);
                 }
+            } catch (error) {
+                if (error.response && error.response.status == 401) {
+                    this.message = error.response.data['message'];
+                    this.showSnackbar = true;
+                }
+            } finally {
+                this.$refs.submit.$el.removeAttribute('disabled');
             }
-
-            this.$refs.submit.$el.removeAttribute('disabled');
         }
     }
 }
