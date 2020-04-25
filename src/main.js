53d5dbbf27e59b88e73ce889298c6a2b91e791fe
VitorMac10 <vitormac3@hotmail.com>
2020-04-25 18:42:32 -0300

    Domain name

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import player from './plugins/media-player.js';
 
-export const api_url = "https://drippy-music.herokuapp.com";
+export const api_url = "https://drippy.live";
 export const socket = io(api_url, { transports: ['websocket'] });
 
 Vue.config.productionTip = false;
