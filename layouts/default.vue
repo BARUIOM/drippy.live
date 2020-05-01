e83de930c7682ae00e30b770de368706ad0507b0
VitorMac10 <vitormac3@hotmail.com>
2020-05-01 20:47:07 -0300

    Useful snackbar & overlay events

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <v-app dark>
+        <v-snackbar v-model="snackbar" :color="status" :top="top" :timeout="4000">{{ message }}</v-snackbar>
         <v-overlay :value="overlay" z-index="100">
             <v-progress-circular indeterminate size="64"></v-progress-circular>
         </v-overlay>
@@ -10,8 +11,21 @@
 <script>
 export default {
     data: () => ({
-        overlay: true
+        overlay: true,
+        snackbar: false,
+        status: 'success',
+        message: '',
+        top: false
     }),
+    created() {
+        this.$root.$on('overlay', value => this.overlay = value);
+        this.$root.$on('snackbar', (message, color, top) => {
+            this.message = message;
+            this.status = color || this.status;
+            if (top !== undefined) this.top = top;
+            this.snackbar = true;
+        });
+    },
     mounted() {
         this.$drippy.validate().then(() => {
             if (this.$route.path.startsWith('/auth')) {
