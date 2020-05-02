83e66ce68c7cbecfb2f09d31bd8db3d1d144615d
VitorMac10 <vitormac3@hotmail.com>
2020-05-01 21:10:34 -0300

    Added action url & email verification view

---
+++ pages/auth/action.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    mounted() {
+        if (this.$route.query['mode']) {
+            sessionStorage['code'] = this.$route.query['oobCode'];
+            switch (this.$route.query['mode']) {
+                case "verifyEmail":
+                    return this.$router.push('/auth/verify');
+            }
+        }
+    }
+}
+</script>
