6086f35bff298173e29c055ac01cf4039023ef31
VitorMac10 <vitormac3@hotmail.com>
2020-05-10 00:17:34 -0300

    Passing index of current track to native player

--- assets/js/native-player.js
+++ assets/js/native-player.js
@@ -3,7 +3,9 @@ import { EventEmitter } from 'events';
 class Player extends EventEmitter {
 
     play(track, track_list) {
-        window.native.play(localStorage['idToken'], localStorage['refreshToken'], JSON.stringify(track), JSON.stringify(track_list));
+        const tracks = [...track_list];
+        const index = tracks.indexOf(tracks.find(e => e['id'] === track['id']));
+        window.native.play(localStorage['idToken'], localStorage['refreshToken'], index, JSON.stringify(tracks));
     }
 
 }
