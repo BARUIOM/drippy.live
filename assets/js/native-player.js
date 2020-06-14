f9334ca3e2b2ac1b7b194cf236d79ac53f26c2b0
VitorMac10 <vitormac3@hotmail.com>
2020-06-14 01:39:57 -0300

    Native player update

--- assets/js/native-player.js
+++ assets/js/native-player.js
@@ -2,14 +2,30 @@ import { EventEmitter } from 'events';
 
 export default (() => {
     if (window.native) {
-        var tracks = [];
+        var _index = 0;
+        var _tracks = [];
 
         class Player extends EventEmitter {
 
-            play(track, track_list) {
-                tracks = [...track_list];
-                const index = tracks.indexOf(tracks.find(e => e['id'] === track['id']));
-                window.native.play(localStorage['idToken'], localStorage['refreshToken'], index, JSON.stringify(tracks));
+            get playlist() {
+                return _tracks;
+            }
+
+            set playlist(value) {
+                _tracks = value;
+            }
+
+            get loaded() {
+                return _tracks.length > 0;
+            }
+
+            get index() {
+                return _index;
+            }
+
+            play(index) {
+                window.native.setData(localStorage['idToken'], localStorage['refreshToken']);
+                window.native.load(_index = index, JSON.stringify(_tracks));
             }
 
             toggle() {
@@ -21,7 +37,7 @@ export default (() => {
         const player = new Player();
         window.native.event = new EventEmitter();
         window.native.event.on('state', value => player.emit('state', value));
-        window.native.event.on('started', index => player.emit('playback-started', tracks[index]));
+        window.native.event.on('started', index => player.emit('playback-started', _tracks[index], index));
         return player;
     }
 })();
