96277a28d013b2e6fe9625a9a0a8076464cf9293
VitorMac10 <vitormac3@hotmail.com>
2020-06-28 14:08:07 -0300

    Drawer layout fixes

--- components/home/Drawer.vue
+++ components/home/Drawer.vue
@@ -1,44 +1,46 @@
 <template>
     <v-layout class="d-flex flex-column overflow-hidden" justify-space-between fill-height>
-        <div>
-            <v-list nav dense>
-                <v-list-item link to="/" exact>
-                    <v-list-item-icon>
-                        <v-icon>mdi-home</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>Home</v-list-item-title>
-                </v-list-item>
-                <v-list-item link to="/collection" exact>
-                    <v-list-item-icon>
-                        <v-icon>mdi-view-list</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>Library</v-list-item-title>
-                </v-list-item>
-            </v-list>
-            <v-subheader>
-                <span>Playlists</span>
-                <v-spacer></v-spacer>
-                <v-btn @click="$root.$emit('create')" icon>
-                    <v-icon>mdi-plus</v-icon>
-                </v-btn>
-            </v-subheader>
-            <v-list class="playlists overflow-y-auto" subheader nav dense>
-                <Playlists v-bind:playlists="playlists.user" v-bind:tooltip="!visible" />
-                <v-divider class="my-1"></v-divider>
-                <Playlists v-bind:playlists="playlists.liked" v-bind:tooltip="!visible" />
-            </v-list>
-        </div>
-        <div>
-            <v-divider></v-divider>
-            <v-list-item class="px-2" @click="$root.$emit('logout')">
-                <v-list-item-avatar>
-                    <v-icon color="red">mdi-exit-to-app</v-icon>
-                </v-list-item-avatar>
+        <v-row no-gutters dense>
+            <v-col cols="12">
+                <v-list nav dense>
+                    <v-list-item link to="/" exact>
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item link to="/collection" exact>
+                        <v-list-item-icon>
+                            <v-icon>mdi-view-list</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Library</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+                <v-subheader>
+                    <span>Playlists</span>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="$root.$emit('create')" icon>
+                        <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                </v-subheader>
+            </v-col>
+            <v-col cols="12">
+                <v-list class="playlists overflow-y-auto" subheader nav dense>
+                    <Playlists v-bind:playlists="playlists.user" v-bind:tooltip="!visible" />
+                    <v-divider class="my-1"></v-divider>
+                    <Playlists v-bind:playlists="playlists.liked" v-bind:tooltip="!visible" />
+                </v-list>
+                <v-divider></v-divider>
+            </v-col>
+        </v-row>
+        <v-list-item class="px-2" @click="$root.$emit('logout')">
+            <v-list-item-avatar>
+                <v-icon color="red">mdi-exit-to-app</v-icon>
+            </v-list-item-avatar>
 
-                <v-list-item-title>Logout</v-list-item-title>
-            </v-list-item>
-            <v-divider></v-divider>
-        </div>
+            <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+        <v-divider></v-divider>
     </v-layout>
 </template>
 
@@ -60,13 +62,13 @@ export default {
 <style lang="scss" scoped>
 @media screen and (max-width: 960px) {
     .playlists {
-        max-height: calc(100vh - 263px);
+        max-height: calc(100vh - 200px);
     }
 }
 
 @media screen and (min-width: 960px) {
     .playlists {
-        max-height: calc(100vh - 335px);
+        max-height: calc(100vh - 272px);
     }
 }
 
@@ -76,26 +78,5 @@ export default {
 
 ::-webkit-scrollbar-thumb {
     background: #363636 !important;
-}
-
-.playlists .icon {
-    display: none;
-}
-
-.v-subheader {
-    transition: visibility 0.1s, opacity 0.25s linear;
-}
-
-.v-navigation-drawer--mini-variant div {
-    overflow: hidden !important;
-
-    .v-subheader {
-        opacity: 0;
-        visibility: hidden;
-    }
-
-    .playlists .icon {
-        display: initial;
-    }
 }
 </style>
