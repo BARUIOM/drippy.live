42f247a82da28928d84ebb9ef4fd959d4b772445
VitorMac10 <vitormac3@hotmail.com>
2020-07-01 19:59:28 -0300

    Basic project structure

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [];
+
+export default new VueRouter({ mode: 'history', base: process.env.BASE_URL, routes });
