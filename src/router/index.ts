7fd65869e15df20fc873ffdace89deab107f2568
VitorMac10 <vitormac3@hotmail.com>
2020-07-01 21:01:48 -0300

    Home page & drawer component replacement

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 
+import Home from '@/views/Home.vue'
+
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [];
+const routes: Array<RouteConfig> = [
+    {
+        path: '/',
+        component: Home,
+        children: []
+    }
+];
 
 export default new VueRouter({ mode: 'history', base: process.env.BASE_URL, routes });
