88f73d1febc9ffc4d96dfeaa6c114d9ae688a744
VitorMac10 <vitormac3@hotmail.com>
2020-07-04 20:32:57 -0300

    Wrapper refactoring & typescript implementation

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import App from '@/App.vue'
 import router from '@/router'
 
 import Drippy, { Manager } from '@/modules/drippy-api'
+const drippy = new Drippy();
 
 import '@/plugins/quasar'
 
 Vue.config.productionTip = false;
-Vue.prototype.$drippy = Drippy;
+Vue.prototype.$drippy = drippy;
 
 Loading.show();
-Drippy.validate().then(async () => {
+drippy.validate().then(async () => {
     Vue.prototype.$user = await Manager.getUser();
 }).finally(() => {
     new Vue({ router, render: h => h(App) }).$mount('#app');
--- src/modules/drippy-api.ts
+++ src/modules/drippy-api.ts
@@ -1,145 +1,145 @@
 import Axios from 'axios'
-
-const user = {
-    profile: {},
-    collection: {
-        playlists: [],
-        following: [],
-        albums: [],
-        tracks: []
-    }
-}
+import User from './drippy-user'
 
 const api_url = '' || 'https://api.drippy.live';
-const exclude = ['/refresh', '/login', '/register'];
 const axios = Axios.create({ baseURL: api_url });
 
 axios.interceptors.request.use(config => {
-    if (!exclude.includes(config.url)) {
-        config.headers['User-Token'] = localStorage['idToken'];
-    }
+    config.headers['User-Token'] = localStorage['idToken'];
     return config;
 });
 
-axios.interceptors.response.use(null, error => {
+axios.interceptors.response.use(e => e, error => {
     if (error.config && error.response && error.response.status === 401) {
-        if (!exclude.includes(error.config.url)) {
-            return axios.post('/refresh', { refresh_token: localStorage['refreshToken'] }).then(response => {
-                localStorage['idToken'] = response.data['id_token'];
-                localStorage['refreshToken'] = response.data['refresh_token'];
-                error.config.headers['User-Token'] = localStorage['idToken'];
-                return Axios.request(error.config);
-            });
-        }
+        return axios.post('/refresh', { refresh_token: localStorage['refreshToken'] }).then(response => {
+            localStorage['idToken'] = response.data['id_token'];
+            localStorage['refreshToken'] = response.data['refresh_token'];
+            error.config.headers['User-Token'] = response.data['id_token'];
+            return Axios.request(error.config);
+        });
     }
     throw error;
 });
 
-export default {
-    async verifyEmail(code) {
-        const response = await axios.get(`/auth/verifyEmail/${code}`);
-        return response.data;
-    },
-    async validate() {
+export default class Drippy {
+
+    public async validate(): Promise<void> {
+        if (!localStorage['idToken'])
+            throw new Error('User not authenticated');
+
         await axios.get('/validate');
-    },
-    async register(email, username, password) {
+    }
+
+    public async register(email: string, username: string, password: string): Promise<MessageResponse> {
         const response = await axios.post('/register', { email, username, password });
-        return response.data;
-    },
-    async login(options = {}) {
+        return response.data as MessageResponse;
+    }
+
+    public async login(options: object | any): Promise<LoginResponse> {
         const response = await axios.post('/login', options);
         localStorage['idToken'] = response.data['idToken'];
         localStorage['refreshToken'] = response.data['refreshToken'];
-        return response.data;
-    },
-    async checkEmail(email) {
+        return response.data as LoginResponse;
+    }
+
+    public async checkEmail(email: string): Promise<MessageCodeResponse> {
         const response = await axios.post('/check_email', { email });
-        return response.data;
-    },
-    async getProfile() {
-        if (!Object.keys(user.profile).length) {
-            return user.profile = (await axios.get('/profile')).data;
-        }
-        return user.profile;
-    },
-    async getFollowedArtists() {
-        if (!user.collection.following.length) {
-            return user.collection.following = (await axios.get('/collection/artists')).data;
-        }
-        return user.collection.following;
-    },
-    async getSavedAlbums() {
-        if (!user.collection.albums.length) {
-            return user.collection.albums = (await axios.get('/collection/albums')).data;
-        }
-        return user.collection.albums;
-    },
-    async getSavedTracks() {
-        if (!user.collection.tracks.length) {
-            return user.collection.tracks = (await axios.get('/collection/tracks')).data;
-        }
-        return user.collection.tracks;
-    },
-    async getPlaylists() {
-        if (!user.collection.playlists.length) {
-            return user.collection.playlists = (await axios.get('/collection/playlists')).data;
-        }
-        return user.collection.playlists;
-    },
-    async getPlaylist(playlist_id) {
+        return response.data as MessageCodeResponse;
+    }
+
+    public async getPlaylist(playlist_id: string) {
         return (await axios.get(`/playlist/${playlist_id}`)).data;
-    },
-    async addTracksToPlaylist(playlist_id, tracks) {
+    }
+
+    public async addTracksToPlaylist(playlist_id: string, tracks: any[]) {
         await axios.post(`/playlist/${playlist_id}/tracks`, { tracks: tracks.map(e => e['id']) });
-    },
-    async removeTrackFromPlaylist(playlist_id, track) {
+    }
+
+    public async removeTrackFromPlaylist(playlist_id: string, track: any) {
         await axios.delete(`/playlist/${playlist_id}/tracks`, { data: { tracks: [track.id] } });
-    },
-    async createPlaylist(name) {
+    }
+
+    public async createPlaylist(name: string, user: User) {
         const playlist = (await axios.post('/collection/playlists', { name })).data;
         user.collection.playlists.unshift(playlist);
         return playlist;
-    },
-    async search(query) {
+    }
+
+    public async search(query: string) {
         const response = (await axios.post('/search', { query })).data;
         window.sessionStorage['search_results'] = JSON.stringify(response);
         return response;
-    },
-    async getArtist(artist_id) {
+    }
+
+    public async getArtist(artist_id: string) {
         return (await axios.get(`/artists/${artist_id}`)).data;
-    },
-    async getAlbum(album_id) {
+    }
+
+    public async getAlbum(album_id: string) {
         const album = (await axios.get(`/albums/${album_id}`)).data;
-        album.tracks.items.forEach(e => e['album'] = { id: album.id, name: album.name, images: album.images });
+        (album.tracks.items as any[]).forEach(e => e['album'] = { id: album.id, name: album.name, images: album.images });
         return album;
-    },
-    async getTrack(track_id) {
+    }
+
+    public async getTrack(track_id: string) {
         return (await axios.get(`/track/${track_id}`)).data;
-    },
-    async spotifyCheck(code) {
+    }
+
+    public async spotifyCheck(code: string): Promise<SpotifyAuthResponse | undefined> {
         const response = await axios.post('/spotify/auth', { code });
-        if (response.data['token']) {
-            return response.data;
-        }
-    },
-    async createSession(track) {
+        if (response.data['token'])
+            return response.data as SpotifyAuthResponse;
+    }
+
+    public async createSession(track: any): Promise<string> {
         const response = await axios.get(`/stream/${track['id']}`);
         return `${api_url}/audio/${response.data['digest']}`;
-    },
-    getPicture(object, index, unset = '/images/music-box-multiple-outline@4x.png') {
-        if (object.images && object.images.length) {
-            return (object.images[index] || object.images[0]).url;
-        }
-        return unset;
-    },
-    isSaved(track) {
-        return user.collection.tracks.some(e => e['id'] === track['id']);
-    },
-    get spotify() {
-        return `${api_url}/spotify`;
-    },
-    get user() {
+    }
+
+    public get url() {
+        return api_url;
+    }
+
+}
+
+export class Manager {
+
+    public static async getUser(): Promise<User> {
+        const user = new User((await axios.get('/profile')).data);
+        user.playlists = (await axios.get('/collection/playlists')).data;
+        user.following = (await axios.get('/collection/artists')).data;
+        user.albums = (await axios.get('/collection/albums')).data;
+        user.tracks = (await axios.get('/collection/tracks')).data;
         return user;
     }
+
+}
+
+export declare interface LoginResponse {
+
+    readonly uid: string
+    readonly email: string
+    readonly displayName: string
+
+    readonly idToken: string
+    readonly refreshToken: string
+
+}
+
+export declare interface MessageResponse {
+
+    readonly message?: string
+
+}
+
+export declare interface MessageCodeResponse extends MessageResponse {
+
+    readonly code: string
+
+}
+
+export declare interface SpotifyAuthResponse extends MessageResponse {
+
+    readonly token: string
+
 }
