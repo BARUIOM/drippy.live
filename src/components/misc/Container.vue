ab2ca1b24502e6ae568a198b4fa8dd2afa95e332
VitorMac10 <vitormac3@hotmail.com>
2020-07-11 13:34:05 -0300

    Container sizing changes

--- src/components/misc/Container.vue
+++ src/components/misc/Container.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <div class="q-pa-sm fit">
-            <div class="q-pa-xs row no-wrap">
-                <div class="col-xs-4 col-sm-2 col-lg-1">
+            <div class="q-pa-xs row">
+                <div
+                    class="col-xs-4 col-sm-3 col-md-2 col-lg-1"
+                    v-bind:class="{ 'col-xs-12': fit, 'col-lg-2': fit }"
+                >
                     <q-img class="shadow-8" :src="thumbnail" :ratio="1" />
                 </div>
                 <div class="q-pa-md col q-gutter-y-sm">
                     <div class="row">
                         <div class="text-h4 text-weight-bold ellipsis" v-text="headline" />
                     </div>
-                    <div v-if="subheader" class="row">
-                        <div class="text-h6" v-text="subheader" />
+                    <div class="row">
+                        <slot name="subheader" />
                     </div>
                     <div class="row q-gutter-x-sm">
                         <slot name="actions" />
@@ -35,8 +38,8 @@ export default class Container extends Vue {
     @Prop({ required: true })
     private thumbnail!: string;
 
-    @Prop()
-    private subheader!: string;
+    @Prop({ default: false })
+    private fit!: boolean;
 
 }
 </script>
