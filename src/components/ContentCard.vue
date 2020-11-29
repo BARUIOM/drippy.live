8e7518ec6d5ecf2ea19a427495edbee70f5f6c99
VitorMac10 <vitormac3@hotmail.com>
2020-11-29 11:26:33 -0300

    Custom shadows

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         @click="$emit('click')"
-        class="card transition-shadow duration-300 cursor-pointer rounded p-4 bg-accent-light dark:bg-accent-dark"
+        class="transition-shadow duration-300 shadow hover:shadow-lg cursor-pointer rounded p-4 bg-accent-light dark:bg-accent-dark"
     >
         <Cover class="shadow" :url="cover" />
         <div
@@ -28,28 +28,3 @@ export default class ContentCard extends Vue {
 
 }
 </script>
-
-<style lang="scss" scoped>
-$shadow-1: rgba(
-    $color: #000000,
-    $alpha: 0.2,
-);
-$shadow-2: rgba(
-    $color: #000000,
-    $alpha: 0.14,
-);
-$shadow-3: rgba(
-    $color: #000000,
-    $alpha: 0.12,
-);
-
-div.card:not(:hover) {
-    box-shadow: 0 1px 5px $shadow-1, 0 2px 2px $shadow-2,
-        0 3px 1px -2px $shadow-3;
-}
-
-div.card:hover {
-    box-shadow: 0 1px 10px $shadow-1, 0 2px 20px $shadow-2,
-        0 3px 10px -2px $shadow-3;
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+const elevation_umbra = 'rgba(0, 0, 0, 0.2)';
+const elevation_penumbra = 'rgba(0, 0, 0, 0.14)';
+const elevation_ambient = 'rgba(0, 0, 0, 0.12)';
+
 module.exports = {
     purge: [],
     darkMode: 'media',
@@ -22,6 +26,12 @@ module.exports = {
                 '5/8': '62.5%',
                 '6/8': '75%',
                 '7/8': '87.5%'
+            },
+            boxShadow: {
+                DEFAULT: `0 1px 5px ${elevation_umbra}, 0 2px 2px ${elevation_penumbra}, 0 3px 1px -2px ${elevation_ambient}`,
+                md: `0 2px 4px -1px ${elevation_umbra}, 0 4px 5px ${elevation_penumbra}, 0 1px 10px ${elevation_ambient}`,
+                lg: `0 3px 5px -1px ${elevation_umbra}, 0 6px 10px ${elevation_penumbra}, 0 1px 18px ${elevation_ambient}`,
+                xl: `0 5px 5px -3px ${elevation_umbra}, 0 8px 10px 1px ${elevation_penumbra}, 0 3px 14px 2px ${elevation_ambient}`
             }
         }
     },
