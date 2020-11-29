ec738771b1b3e30bed6637421c56f419a96a5262
VitorMac10 <vitormac3@hotmail.com>
2020-11-29 07:25:14 -0300

    Header/Footer shadows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="flex flex-col h-screen text-black dark:text-white">
-        <header class="shadow-md bg-accent-light dark:bg-accent-dark"></header>
+        <header class="bg-accent-light dark:bg-accent-dark"></header>
         <main
             class="flex flex-wrap h-full bg-main-light dark:bg-main-dark overflow-x-hidden overflow-y-auto"
         >
             <router-view />
         </main>
-        <footer class="shadow-md bg-accent-light dark:bg-accent-dark"></footer>
+        <footer class="bg-accent-light dark:bg-accent-dark"></footer>
     </div>
 </template>
 
@@ -20,11 +20,26 @@ export default class Home extends Vue {
 </script>
 
 <style lang="scss">
+$shadow-1: 0 0 10px 2px
+    rgba(
+        $color: #000000,
+        $alpha: 0.2,
+    );
+$shadow-2: 0 0 10px
+    rgba(
+        $color: #000000,
+        $alpha: 0.24,
+    );
+
 header {
+    z-index: 10;
     flex: 0 0 54px;
+    box-shadow: $shadow-1, $shadow-2;
 }
 
 footer {
+    z-index: 10;
     flex: 0 0 72px;
+    box-shadow: $shadow-1, $shadow-2;
 }
 </style>
