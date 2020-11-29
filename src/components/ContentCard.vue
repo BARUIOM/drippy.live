4383f2f00a63584a1e1eebe02a3dbea388b2915d
VitorMac10 <vitormac3@hotmail.com>
2020-11-29 07:05:44 -0300

    Custom card shadows

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         @click="$emit('click')"
-        class="transition-shadow cursor-pointer shadow rounded p-4 bg-accent-light dark:bg-accent-dark hover:shadow-xl"
+        class="card transition-shadow duration-300 cursor-pointer rounded p-4 bg-accent-light dark:bg-accent-dark"
     >
         <Cover class="shadow" :url="cover" />
         <div
@@ -28,3 +28,28 @@ export default class ContentCard extends Vue {
 
 }
 </script>
+
+<style lang="scss" scoped>
+$shadow-1: rgba(
+    $color: #000000,
+    $alpha: 0.2,
+);
+$shadow-2: rgba(
+    $color: #000000,
+    $alpha: 0.14,
+);
+$shadow-3: rgba(
+    $color: #000000,
+    $alpha: 0.12,
+);
+
+div.card:not(:hover) {
+    box-shadow: 0 1px 5px $shadow-1, 0 2px 2px $shadow-2,
+        0 3px 1px -2px $shadow-3;
+}
+
+div.card:hover {
+    box-shadow: 0 1px 10px $shadow-1, 0 2px 20px $shadow-2,
+        0 3px 10px -2px $shadow-3;
+}
+</style>
