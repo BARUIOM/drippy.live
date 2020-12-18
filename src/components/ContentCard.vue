8c84f69846f866f484e0cfd68a64e9e0da6ae3cf
listanegra <vitormac3@hotmail.com>
2020-12-18 19:03:51 -0300

    Content card border radius on mobile

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -3,7 +3,7 @@
         @click="$emit('click')"
         class="transition-shadow duration-300 shadow hover:shadow-lg cursor-pointer rounded bg-accent-light dark:bg-accent-dark md:p-2"
     >
-        <Cover class="shadow md:m-2" :url="cover" />
+        <Cover class="shadow rounded-t md:rounded-none md:m-2" :url="cover" />
         <div
             class="text-md md:text-xl text-center font-bold truncate p-4 md:p-0 md:m-2"
             v-text="title"
