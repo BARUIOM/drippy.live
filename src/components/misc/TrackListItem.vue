0619f0cab6428282c763bb45807e29cc38dcc48d
listanegra <vitormac3@hotmail.com>
2020-12-17 21:27:07 -0300

    Tracklist item responsive layout

--- src/components/misc/TrackListItem.vue
+++ src/components/misc/TrackListItem.vue
@@ -2,28 +2,27 @@
     <div
         @click="$emit('click')"
         @contextmenu.prevent="menu = true"
-        class="item flex w-full cursor-pointer select-none hover:bg-opacity-10 hover:bg-black dark:hover:bg-white p-2"
-        style="height: 64px"
+        class="item cursor-pointer w-full h-16 hover:bg-opacity-10 hover:bg-black dark:hover:bg-white"
     >
-        <div class="flex w-10/12 md:w-6/12 h-full items-center">
-            <div
-                class="flex w-2/12 md:w-1/12 h-full justify-end items-center px-4"
-            >
+        <div class="flex items-center h-full select-none p-2">
+            <div class="flex items-center justify-center w-8 h-full mx-2">
                 <span class="mdi mdi-play mdi-24px" />
                 <span v-text="index + 1" />
             </div>
-            <div class="w-10/12 md:w-11/12">
+            <div v-if="!album" class="mx-2">
+                <Cover
+                    class="shadow"
+                    size="48px"
+                    :url="Utils.get(item.album.images, 'small')"
+                />
+            </div>
+            <div class="flex-1 min-w-0">
                 <div class="truncate" v-text="item.name" />
                 <HyperLink :elements="Utils.map(item.artists, 'artist')" />
             </div>
-        </div>
-        <div v-if="!album" class="hidden md:flex w-4/12 items-center">
-            <HyperLink :elements="Utils.map([item.album], 'album')" />
-        </div>
-        <div
-            v-bind:class="{ 'md:w-6/12': album }"
-            class="flex w-2/12 justify-end items-center"
-        >
+            <div v-if="!album" class="hidden md:block flex-1 min-w-0">
+                <HyperLink :elements="Utils.map([item.album], 'album')" />
+            </div>
             <div class="menu-section mx-4">
                 <Button size="2rem">
                     <span class="mdi mdi-dots-horizontal mdi-24px" />
@@ -33,13 +32,6 @@
                 <span
                     class="text-opacity-40 text-black dark:text-white"
                     v-text="Utils.format(item.duration_ms)"
-                />
-            </div>
-            <div v-if="!album">
-                <Cover
-                    class="shadow"
-                    size="48px"
-                    :url="Utils.get(item.album.images, 'small')"
                 />
             </div>
         </div>
