ee535dee8d05189c935b08456bc50c4bef411e53
listanegra <vitormac3@hotmail.com>
2020-12-20 13:05:35 -0300

    Routing fixes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 
 import Home from '@/views/Home.vue'
+
+import Auth from '@/views/auth/Auth.vue'
 import Login from '@/views/auth/Login.vue'
 import Spotify from '@/views/auth/Spotify.vue'
 
@@ -27,41 +29,48 @@ const routes: Array<RouteConfig> = [
             },
             {
                 props: true,
-                path: '/search',
+                path: 'search',
                 name: 'search',
                 component: Search
             },
             {
-                path: '/artist/:id',
+                path: 'artist/:id',
                 name: 'artist',
                 component: Artist
             },
             {
-                path: '/album/:id',
+                path: 'album/:id',
                 name: 'album',
                 component: Album
             },
             {
-                path: '/track/:id',
+                path: 'track/:id',
                 name: 'track',
                 component: Track
             },
             {
-                path: '/playlist/:id',
+                path: 'playlist/:id',
                 name: 'playlist',
                 component: Playlist
             }
         ]
     },
     {
-        name: 'login',
-        path: '/auth/login',
-        component: Login
-    },
-    {
-        name: 'spotify',
-        path: '/auth/spotify',
-        component: Spotify
+        path: '/auth',
+        name: 'auth',
+        component: Auth,
+        children: [
+            {
+                path: 'login',
+                name: 'login',
+                component: Login
+            },
+            {
+                path: 'spotify',
+                name: 'spotify',
+                component: Spotify
+            }
+        ]
     }
 ];
 
---
+++ src/views/auth/Auth.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-view />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Auth extends Vue {
+
+    private mounted() {
+        if (this.$route.name === 'auth') {
+            this.$router.push({ name: 'login' });
+        }
+    }
+
+}
+</script>
